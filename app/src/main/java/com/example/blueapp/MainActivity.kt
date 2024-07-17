f8c2dd1a9f4ee6109134228e533a56aa52af6261
PeterG3POGamer <pepeterbryan@gmail.com>
2024-07-17 11:52:25 -0500

    Update Version: v0.1.8.1 * Actualización OFF temporalmente

--- app/src/main/java/com/example/blueapp/MainActivity.kt
+++ app/src/main/java/com/example/blueapp/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.example.blueapp.VersionControl.UpdateChecker
 import com.example.blueapp.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
