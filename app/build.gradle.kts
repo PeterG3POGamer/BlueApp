236ba1aefb1298cafa4345f20edd812238375d85
PeterG3POGamer <pepeterbryan@gmail.com>
2024-09-13 14:29:57 -0500

    Update Version: v1.13.2 * Fix cambio de URL;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.13.1"
+        versionName = "1.13.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "app.serlanventas.mobile"
     }
