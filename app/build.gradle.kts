76a0b892346da6612649b56d8045a5441d972484
PeterG3POGamer <pepeterbryan@gmail.com>
2024-09-16 08:55:17 -0500

    Update Version: v1.13.5 * Fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.13.5"
+        versionName = "1.13.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "app.serlanventas.mobile"
     }
