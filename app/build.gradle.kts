a8f2ea9efbeeb5997e952ac90e08139ab13d489d
PeterG3POGamer <pepeterbryan@gmail.com>
2024-09-16 17:39:11 -0500

    Update Version: v1.13.10 + Add Crash Log v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.13.9"
+        versionName = "1.13.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "app.serlanventas.mobile"
     }
