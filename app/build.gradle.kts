821923546e7f3a824d1a95d7785cb23f9e80c133
PeterG3POGamer <pepeterbryan@gmail.com>
2024-09-19 16:46:48 -0500

    Update Version: v1.14 * Mejora de diseño; * Fix input del peso bloqueado;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.13.16"
+        versionName = "1.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "app.serlanventas.mobile"
         vectorDrawables {
