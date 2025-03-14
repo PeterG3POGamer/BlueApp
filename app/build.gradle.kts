37ff9c1f283992d635a88c85094865f2b7d48fbc
PeterG3POGamer <pepeterbryan@gmail.com>
2025-03-14 17:42:27 -0500

    Update Version: v1.17.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "1.17.10"
+        versionName = "1.17.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "app.serlanventas.mobile"
         vectorDrawables {
